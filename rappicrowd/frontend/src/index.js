8e80afd36421a5e9e8f94f1e1979480ae92f7b8b
oran2527 <orlago250183@gmail.com>
2020-06-02 00:06:16 +0000

    erase

--- rappicrowd/frontend/src/index.js
+++
@@ -1,2 +0,0 @@
-import App from "./components/App";
-import Como_funciona_domiciliario from "./components/Como_funciona_domiciliario";
--- rappicrowd/frontend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path(r'front', views.index ),
-    path(r'como_funciona_domiciliario', views.como_funciona_domiciliario ),
-]
