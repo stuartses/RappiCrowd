7e705753059a9b90f8d676cd820e7ab284361ce2
oran2527 <orlago250183@gmail.com>
2020-06-03 19:28:13 +0000

    erase rappi

--- rappicrowd/frontend/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'frontend/index.html')
-
-def como_funciona_domiciliario(request):
-    return render(request, 'frontend/como_funciona_domiciliario.html')    
