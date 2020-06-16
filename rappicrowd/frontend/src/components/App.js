681b95d2e4ab8ec08b20322d764171c835156f5b
dfbq91 <dfbq91@gmail.com>
2020-06-16 12:23:57 -0500

    404 error, solicitante information view for inversor and solicitante page after login implemented

--- rappicrowd/frontend/src/components/App.js
+++ rappicrowd/frontend/src/components/App.js
@@ -12,6 +12,9 @@ import RegistroInversor from '../pages/RegistroInversor';
 import RegistroDomiciliario from '../pages/RegistroDomiciliario';
 import SolicitanteList from '../pages/SolicitanteList';
 import Perfilamiento from '../pages/Perfilamiento';
+import InvertirEn from '../pages/InvertirEn';
+import SolicitanteURL from '../pages/SolicitanteURL';
+import NotFound from '../pages/NotFound';
 
 
 import { Provider } from 'react-redux';
@@ -22,19 +25,22 @@ class App extends Component {
     return (
       <Provider store={store}>  	    
         <BrowserRouter>
-        <Layout>
-	  <Switch>
-	    <Route exact path='/' component={Landing}></Route>
-	    <Route exact path='/como-funciona-domiciliario' component={ExplicacionDomiciliario}></Route>  
-	    <Route exact path='/como-funciona-inversor' component={ExplicacionInversor}></Route>  
-	    <Route exact path='/registro-inversor' component={RegistroInversor}></Route>
-	    <Route exact path='/login-inversor' component={LoginInversor}></Route>
-	    <Route exact path='/registro-domiciliario' component={RegistroDomiciliario}></Route>
-	    <Route exact path='/login-solicitante' component={LoginSolicitante}></Route>
-	    <Route exact path='/listado-domiciliarios' component={SolicitanteList}></Route>
-	    <Route exact path='/perfilamiento' component={Perfilamiento}></Route>
-	  </Switch>    
-        </Layout>	 
+        	<Layout>
+				<Switch>
+					<Route exact path='/' component={Landing}></Route>
+					<Route exact path='/como-funciona-domiciliario' component={ExplicacionDomiciliario}></Route>  
+					<Route exact path='/como-funciona-inversor' component={ExplicacionInversor}></Route>  
+					<Route exact path='/registro-inversor' component={RegistroInversor}></Route>
+					<Route exact path='/login-inversor' component={LoginInversor}></Route>
+					<Route exact path='/registro-domiciliario' component={RegistroDomiciliario}></Route>
+					<Route exact path='/login-solicitante' component={LoginSolicitante}></Route>
+					<Route exact path='/listado-domiciliarios' component={SolicitanteList}></Route>
+					<Route exact path='/perfilamiento' component={Perfilamiento}></Route>
+					<Route exact path='/invertir-en/:ced' component={InvertirEn}></Route>
+					<Route exact path='/solicitante/:ced' component={SolicitanteURL}></Route>
+					<Route component={NotFound}></Route>
+				</Switch>    
+        	</Layout>	 
         </BrowserRouter>	    
       </Provider>      	    
     )
--- rappicrowd/frontend/src/pages/LoginSolicitante.js
+++ rappicrowd/frontend/src/pages/LoginSolicitante.js
@@ -1,97 +1,126 @@
+/* Este componente contiene la pantalla de login para
+el solicitante. Se encuentra en /login-solicitante */
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getWorkers } from '../actions/workers';
 
- 
 import './styles/Login.css'
 
 
 class LoginSolicitante extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			workerCed: "",
+			workerPassword: ""
+		};
 
-	    constructor(props) {
-              super(props);
-              this.state = {
-                workerrCed: "",
-		workerPassword: ""
-	      };
+	this.SubmitHandler = this.SubmitHandler.bind(this);
+	}
 
-              this.SubmitHandler = this.SubmitHandler.bind(this);
-	    }
+	changeHandler = e => {
+		/* Asigna lo digitado en el form a los atributos de state */
+		this.setState({ [e.target.name]: e.target.value });
+		console.log(e.target);
+	}
 
-	    changeHandler = e => {
-	      this.setState({ [e.target.name]: e.target.value });
-	      console.log(e.target);
-	    }
+	SubmitHandler = e => {
+		/* Se ejecuta al hacer click en iniciar sesión */
+		e.preventDefault();
+		const cedulaDigitada = this.state.workerCed;
+		const personaEncontrada = this.props.workers.filter(persona => {
+			return persona.workerCed === cedulaDigitada;
+		});
+		this.props.history.push(`/solicitante/${personaEncontrada[0].workerCed}`);
+	};
 
-	    SubmitHandler = e => {
-	      e.preventDefault();
-	      this.props.history.push('/');
-	    };
+	static propTypes = {
+		workers: PropTypes.array.isRequired
+	};
 
-	    static propTypes = {
-	      workers: PropTypes.array.isRequired
-            };
-
-            componentDidMount() {
-              this.props.getWorkers();
-            }
-
+	componentDidMount() {
+		this.props.getWorkers();
+	}
 	    
+	render() {
+		return(
+			<React.Fragment>
+				<form method="post" onSubmit={this.SubmitHandler}>
+					<div className='background'>
+						<div className="container my-5">
+							<div className="row">
+								<div className="col-md-5 mx-auto">
+									<div id="first">
+										<div className="myform form ">
+											<div className="logo mb-3">
+												<div className="col-md-12 text-center">
+													<h1>Iniciar sesión</h1>
+												</div>
+											</div>
+											<div className="form-group">
+												<label htmlFor="exampleInputEmail1">Cédula</label>
+												<input
+													type="input"
+													name="workerCed"
+													className="form-control"
+													placeholder="Ingresa cédula"
+													onChange={this.changeHandler}
+													value={this.state.workerCed}/>
+											</div>
+											<div className="form-group">
+												<label htmlFor="exampleInputEmail1">Contraseña</label>
+												<input
+													type="password"
+													name="workerPassword"
+													className="form-control"
+													placeholder="Ingresa contraseña"
+													onChange={this.changeHandler}
+													value={this.state.workerPassword}/>
+											</div>
 
-	    
-	    render() {
-		            return(
-				        <React.Fragment>
-                                            <form method="post" onSubmit={this.SubmitHandler}>
-				                <div className='background'>
-				                    <div className="container my-5">
-				                        <div className="row">
-				                            <div className="col-md-5 mx-auto">
-				                                <div id="first">
-				                                    <div className="myform form ">
-				                                        <div className="logo mb-3">
-				                                            <div className="col-md-12 text-center">
-				                                                <h1>Iniciar sesión</h1>
-				                                            </div>
-				                                        </div>
-				                                        <form action="" method="post" name="login">
-				                                            <div className="form-group">
-				                                                <label for="exampleInputEmail1">Correo electrónico</label>
-				                                                <input type="email" name="email"  className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>
-				                                            </div>
-				                                            <div className="form-group">
-				                                                <label for="exampleInputEmail1">Contraseña</label>
-				                                                <input type="password" name="password" id="password"  className="form-control" aria-describedby="emailHelp" placeholder="Enter Password"/>
-				                                            </div>
-				                                            <div className="form-group">
-				                                                <p className="text-center">Al iniciar sesión, usted está aceptando <a href="#">Nuestros términos de uso</a></p>
-				                                            </div>
-				                                            <div className="col-md-12 text-center ">
-				                                                <button to='' type="submit" className=" btn btn-block mybtn btn-primary tx-tfm">Iniciar sesión</button>
-				                                            </div>
-				                                            <div className="col-md-12 ">
-				                                                <div className="login-or">
-				                                                    <hr className="hr-or"></hr>
-				                                                    <span className="span-or">or</span>
-				                                                </div>
-				                                            </div>
-				                                            
-				                                            <div className="form-group">
-				                                                <p className="text-center">¿No tienes cuenta?<a href='/registro-domiciliario' id="signup">Regístrate</a></p>
-				                                            </div>
-				                                        </form>
-				                                    </div>
-				                                </div>
-				                            </div>
-				                        </div>
-				                    </div>
-				                </div>
-				            </form>
-				        </React.Fragment>    
-				            )
-		        }
+											<div className="form-group">
+												<p className="text-center">
+													Al iniciar sesión, usted está aceptando
+													<a href="#">Nuestros términos de uso</a>
+												</p>
+											</div>
+
+											<div className="col-md-12 text-center">
+												<button
+													to=''
+													type="submit"
+													className=" btn btn-block mybtn btn-primary tx-tfm">Iniciar sesión
+												</button>
+											</div>
+
+											<div className="col-md-12 ">
+												<div className="login-or">
+													<hr className="hr-or"></hr>
+													<span className="span-or">or</span>
+												</div>
+											</div>
+											
+											<div className="form-group">
+												<p className="text-center">¿No tienes cuenta?
+													<Link
+														to='/registro-domiciliario'
+														id="signup">Regístrate
+													</Link>
+												</p>
+											</div>
+
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</form>
+			</React.Fragment>    
+		)
+	}
 }
 
 const mapStateToProps = state => ({
