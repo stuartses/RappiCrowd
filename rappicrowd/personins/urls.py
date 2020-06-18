28ba74aacdf2ddd173793104d32bfb6d4da90b45
oran2527 <orlago250183@gmail.com>
2020-06-18 16:18:46 +0000

    erase

--- rappicrowd/personins/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'personins'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('load/', views.load, name='load'),
-]
