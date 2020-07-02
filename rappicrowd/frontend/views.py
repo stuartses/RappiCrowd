fab364557188927ed2a29192c0185dff718f989d
stuartses <stuart.ses@hotmail.com>
2020-07-02 18:44:21 -0500

    Force csrf cookie

--- rappicrowd/frontend/views.py
+++ rappicrowd/frontend/views.py
@@ -1,5 +1,7 @@
 ''' This is the view to show the components of the frontend '''
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 
+@ensure_csrf_cookie
 def index(request):
     return render(request, 'frontend/index.html')
